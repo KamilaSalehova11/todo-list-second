ce26107413ddbda11984e23db61b76ca0368ee60
kamila <kamilasalehova11@gmail.com>
2026-04-24 15:48:27 -0400

    lesson 3 done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,25 @@
 /** @format */
+
 import "./App.css";
 import TodoList from "./TodoList.jsx";
 import TodoForm from "./TodoForm.jsx";
+import { useState } from "react";
+
+const todos = [
+	{ id: 1, title: "review resources" },
+	{ id: 2, title: "take notes" },
+	{ id: 3, title: "code out app" },
+	{ id: 4, title: "marked check up" },
+];
 
 function App() {
+	const [todoList, setTodoList] = useState(todos);
 
 	return (
 		<div>
 			<h1>My Todos</h1>
-			<TodoForm /> 
-			<TodoList /> 
+			<TodoForm />
+			<TodoList todoList={todoList} />
 		</div>
 	);
 }
