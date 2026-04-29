a8116c10d70c5da8456ec2f068f0c55039339a4c
kamila <kamilasalehova11@gmail.com>
2026-04-29 16:56:48 -0400

    focused function added

--- vite-project/src/TodoForm.jsx
+++ vite-project/src/TodoForm.jsx
@@ -1,16 +1,30 @@
-/** @format */
+//import { useState } from "react";
+import { useRef, useState } from "react";
 
-function TodoForm() {
+function TodoForm({ onAddTodo }) {
+	const [workingTodoTitle, setWorkingTodoTitle] = useState("");
+	const todoTitleInput = useRef(null);
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		onAddTodo(workingTodoTitle);
+		setWorkingTodoTitle("");
+		todoTitleInput.current.focus();
+	}
+
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<label htmlFor="todoTitle">Todo</label>
 			<input
 				type="text"
 				id="todoTitle"
+				value={workingTodoTitle}
+				onChange={(event) => setWorkingTodoTitle(event.target.value)}
+				ref={todoTitleInput}
 			/>
 			<button
 				type="submit"
-				disabled
+				disabled={workingTodoTitle === ""}
 			>
 				Add Todo
 			</button>
--- vite-project/src/TodoList.jsx
+++ vite-project/src/TodoList.jsx
@@ -1,4 +1,3 @@
-/** @format */
 
 import TodoListItem from "./TodoListItem.jsx";
 
